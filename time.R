52503d196b07928ea6b05a1035715fb6c4b119ed
Marcin Rybarczyk <rybarczyk.m@gmail.com>
2019-09-18 21:36:52 +0200

    Update time.R

--- time.R
+++ time.R
@@ -1,16 +1,17 @@
 library(readr)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
-titles <- read.csv("data/titles.csv")
-stats <- read.csv("data/stats.csv")
+titles <- read_csv("data/titles.csv")
+stats <- read_csv("data/stats.csv")
 
 books <- full_join(titles, stats,by = "id")
   
 dickens <- filter(books, str_detect(author, 'Dickens'))
 dickens_stats<- dickens %>% select(id, words, sentences, to_be_verbs, contractions, pauses, cliches, similes)
 
-published <- read.csv("data/published.csv")
+published <- read_csv("data/published.csv")
 
 time <- full_join(dickens_stats, published)
 
